23ab827c0facfe3f69a8d1268bc672d1d8a3390f
Heru Candra <herucandra272@gmail.com>
2025-12-10 20:39:09 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,47 @@
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+import os
+
+# Argument parser untuk CI
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_estimators", type=int, default=100)
+parser.add_argument("--max_depth", type=int, default=10)
+args = parser.parse_args()
+
+# Load data
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, "lung_cancer_preprocessing.csv")
+df = pd.read_csv(data_path)
+
+X = df.drop(['LUNG_CANCER', 'AGE_GROUP'], axis=1)
+y = df['LUNG_CANCER']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=args.n_estimators,
+        max_depth=args.max_depth,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    # Logging manual
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_param("max_depth", args.max_depth)
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(model, name="model")
+
+    print(f"Training selesai. Accuracy = {acc:.4f}")
